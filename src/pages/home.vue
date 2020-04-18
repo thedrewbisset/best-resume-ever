e00dd3a3e3a97d720424f60b16b2086f8a9f3efd
thedrewbisset <thedrewbisset@gmail.com>
2020-04-18 00:51:42 -0400

    Remove unused templates, pdfs, and pngs
    - Preserve pdf/ directory but ignore contents
    - Remove extraneous vue templates
    - Add preview asset for material-dark-extended
    - Remove extraneous preview assets
    - Pare down home page to single resume

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,80 +1,12 @@
 <template>
 <div class="home">
-  <a href="https://github.com/salomonelli/best-resume-ever" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
-  <div class="logo">
-    <img src="../assets/logo.png" />
-  </div>
   <h3 class="title">best-resume-ever</h3>
   <div class="previews">
     <div class="preview">
-      <router-link v-bind:to="'/resume/material-dark'">
+      <router-link v-bind:to="'/resume/material-dark-extended'">
         <div class="preview-wrapper">
-          <img src="../assets/preview/resume-material-dark.png" />
-          <span>material-dark</span>
-        </div>
-      </router-link>
-    </div>
-    <div class="preview">
-      <router-link v-bind:to="'/resume/left-right'">
-        <div class="preview-wrapper">
-          <img src="../assets/preview/resume-left-right.png" />
-          <span>left-right</span>
-        </div>
-      </router-link>
-    </div>
-    <div class="preview">
-      <router-link v-bind:to="'/resume/oblique'">
-        <div class="preview-wrapper">
-          <img src="../assets/preview/resume-oblique.png" />
-          <span>oblique</span>
-        </div>
-      </router-link>
-    </div>
-    <div class="preview">
-      <router-link v-bind:to="'/resume/side-bar'">
-        <div class="preview-wrapper">
-          <img src="../assets/preview/resume-side-bar.png" />
-          <span>side-bar</span>
-        </div>
-      </router-link>
-    </div>
-    <div class="preview">
-      <router-link v-bind:to="'/resume/purple'">
-        <div class="preview-wrapper">
-          <img src="../assets/preview/resume-purple.png" />
-          <span>purple</span>
-        </div>
-      </router-link>
-    </div>
-    <div class="preview">
-      <router-link v-bind:to="'/resume/side-bar-rtl'">
-        <div class="preview-wrapper">
-          <img src="../assets/preview/resume-side-bar-rtl.png" />
-          <span>side-bar-rtl</span>
-        </div>
-      </router-link>
-    </div>
-    <div class="preview">
-      <router-link v-bind:to="'/resume/left-right-rtl'">
-        <div class="preview-wrapper">
-          <img src="../assets/preview/resume-left-right-rtl.png" />
-          <span>left-right-rtl</span>
-        </div>
-      </router-link>
-    </div>
-    <div class="preview">
-      <router-link v-bind:to="'/resume/creative'">
-        <div class="preview-wrapper">
-          <img src="../assets/preview/resume-creative.png" />
-          <span>creative</span>
-        </div>
-      </router-link>
-    </div>
-    <div class="preview">
-      <router-link v-bind:to="'/resume/cool'">
-        <div class="preview-wrapper">
-          <img src="../assets/preview/resume-cool.png" />
-          <span>cool</span>
+          <img src="../assets/preview/resume-material-dark-extended.png" />
+          <span>material-dark-extended</span>
         </div>
       </router-link>
     </div>
--- src/resumes/resumes.js
+++ src/resumes/resumes.js
@@ -1,11 +1,2 @@
 // If you create a new resume, import it here:
-import './material-dark.vue';
 import './material-dark-extended.vue';
-import './left-right.vue';
-import './oblique.vue';
-import './side-bar.vue';
-import './purple.vue';
-import './side-bar-rtl.vue';
-import './left-right-rtl.vue';
-import './creative.vue';
-import './cool.vue';
